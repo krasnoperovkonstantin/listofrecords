a4500be40bfce1b1a59c71e31ada905830f94163
Krasnoperov Konstantin <krasnoperovkonstantin@gmail.com>
2021-01-04 19:39:05 +0400

    изменил структуру полей

--- resources/views/insert.blade.php
+++ resources/views/insert.blade.php
@@ -11,15 +11,27 @@
   @csrf
   <div class="form-group">
     <label for="name">{{ __('face.name') }}</label>
-    <input type="text" name="name"  placeholder="Введите название пластинки" id="name" class="form-control">
-  </div>
-  <div class="form-group">
-    <label for="author">{{ __('face.author') }}</label>
-    <input type="text" name="author"  placeholder="Введите автора пластинки" id="author" class="form-control">
+    <input type="text" name="name" value="" placeholder="Введите название пластинки" id="name" class="form-control">
   </div>
   <div class="form-group">
     <label for="genre">{{ __('face.genre') }}</label>
-    <input type="text" name="genre"  placeholder="Введите жанр пластинки" id="genre" class="form-control">
+    <input type="text" name="genre" value="" placeholder="Введите автора пластинки" id="genre" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="format">{{ __('face.format') }}</label>
+    <input type="text" name="format" value="" placeholder="Введите жанр пластинки" id="format" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="origin">{{ __('face.origin') }}</label>
+    <input type="text" name="origin" value="" placeholder="Введите жанр пластинки" id="origin" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="releasedate">{{ __('face.releasedate') }}</label>
+    <input type="text" name="releasedate" value="" placeholder="Введите жанр пластинки" id="releasedate" class="form-control">
+  </div>
+  <div class="form-group">
+    <label for="manufacturer">{{ __('face.manufacturer') }}</label>
+    <input type="text" name="manufacturer" value="" placeholder="Введите жанр пластинки" id="manufacturer" class="form-control">
   </div>
   <div class="form-group">
     <label for="listoftracks">{{ __('face.listoftracks') }}</label>
